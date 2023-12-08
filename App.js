47e8a2c4a76d83fac254f26a7b1a032c57f0c08f
MuskanVerma11 <muskanvermabsp@gmail.com>
2023-12-08 23:19:28 +0530

    Your commit message here

--- App.js
+++ App.js
@@ -1,19 +1,11 @@
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import Feedback from './screens/Feedback';
+import Login from './screens/Login';
+import Navigation from './Navigation/Navigation';
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <Feedback/>
-    </SafeAreaView>
+      <Navigation/>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
